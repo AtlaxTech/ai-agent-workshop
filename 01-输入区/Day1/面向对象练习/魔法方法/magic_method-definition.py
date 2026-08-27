eda3d9947e17b9d997dbbb800b41b9d5938cee7e
AtlaxTech <atlax-tech@outlook.com>
2026-08-27 14:07:07 +0800

    面向对象：魔法方法
    Signed-off-by: AtlaxTech <atlax-tech@outlook.com>

---
+++ 01-输入区/Day1/面向对象练习/魔法方法/magic_method-definition.py
@@ -0,0 +1,26 @@
+class Car:
+    def __init__(self, brand, name, price):
+        self.brand = brand
+        self.name = name
+        self.price = price
+
+    def running(self):
+        print(f"{self.brand} {self.name} 正在高速行驶中")
+
+    def __str__(self):  # 将当前对象转换成字符串 默认是输出地址
+        return f"{self.brand} {self.name} {self.price}"
+
+    def __eq__(self, other):
+        return self.price == other.price and self.brand == other.brand and self.name == other.name
+
+    def __lt__(self, other):
+        return self.price < other.price
+
+
+c1 = Car("BMW", "X5", 500000)
+print(c1)
+c2 = Car("BMW", "X5", 500000)
+print(c2)
+
+print(c1 == c2)
+print(c1 < c2)
